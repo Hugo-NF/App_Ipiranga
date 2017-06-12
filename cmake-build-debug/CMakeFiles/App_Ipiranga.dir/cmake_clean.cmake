a60315b6188bae6055a141d0fe3d1e15a61c7e97
Hugo-NF <hugonfonseca@hotmail.com>
2017-06-12 14:03:42 -0500

    29 tentativa de dar commit

---
+++ cmake-build-debug/CMakeFiles/App_Ipiranga.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/App_Ipiranga.dir/src/main.cpp.o"
+  "CMakeFiles/App_Ipiranga.dir/src/User.cpp.o"
+  "App_Ipiranga.pdb"
+  "App_Ipiranga"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/App_Ipiranga.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
